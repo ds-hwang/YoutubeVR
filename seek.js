b1b128a5b2748317cb969bfd5d2eb22677001b1a
Dongseong Hwang <dongseong.hwang@intel.com>
2016-04-22 16:43:03 +0300

    make sane scale

--- seek.js
+++ seek.js
@@ -10,28 +10,45 @@ function getPlayer() {
     return document.getElementById("player-api");
 }
 
+function getPlayerHolder() {
+    return document.getElementById("placeholder-player");
+}
+
 function toggleFullscreen(fullscreen) {
     var player = getPlayer();
     var player_style = player.style;
+    var holder_style = getPlayerHolder().style;
     if (fullscreen) {
+        document.body.style.overflow = "hidden"
+
         player_style.position = "absolute";
         var rect = player.getBoundingClientRect();
         var transform_scalew = window.innerWidth / rect.width;
         var transform_scaleh = window.innerHeight / rect.height;
-        player_style.transform = "scale(" + transform_scalew + ", " + transform_scaleh +")";
+        var transform_scale = Math.min(transform_scalew, transform_scaleh);
+        player_style.transform = "scale(" + transform_scale + ", " + transform_scale +")";
         rect = player.getBoundingClientRect();
         var target_x = (window.innerWidth - rect.width) / 2;
         var target_y = (window.innerHeight - rect.height) / 2;
         //player_style.left = (target_x - rect.left) + "px";
         player_style.transform = "translate(" + (target_x - rect.left) + "px , " +
+            (target_y - rect.top) + "px) scale(" + transform_scale + ", " + transform_scale +")";
+        player_style.zIndex = "2000000001";
+
+        holder_style.position = "absolute";
+        holder_style.transform = "translate(" + (target_x - rect.left) + "px , " +
             (target_y - rect.top) + "px) scale(" + transform_scalew + ", " + transform_scaleh +")";
-        player_style.zIndex = "19999999999999999999999";
-        document.body.style.overflow = "hidden"
+        holder_style.zIndex = "2000000000";
     } else {
+        document.body.style.overflow = ""
+
         player_style.transform = "";
         player_style.zIndex = "";
         player_style.position = "";
-        document.body.style.overflow = ""
+
+        holder_style.position = "";
+        holder_style.transform = "";
+        holder_style.zIndex = "";
     }
 }
 
