38648634b4a82deb2c117e899a1e59058aa72bde
Dongseong Hwang <dongseong.hwang@intel.com>
2018-02-21 22:56:43 -0800

    indentation by space 2

--- eventPage.js
+++ eventPage.js
@@ -9,51 +9,48 @@ var isResUpdated = false;
 var currentTab = null;
 
 // Sets the page icon for youtube.com and changes from max to min icons.
-function setIconForYouTube(tabId, changeInfo, tab) {
-	toggleIcon(tab , false);
-}
+function setIconForYouTube(tabId, changeInfo, tab) { toggleIcon(tab, false); }
 
 function toggleIcon(tab, fullscreen) {
-    if (tab.url.indexOf("youtube.com") > 0 ){
-		if (fullscreen){
-		    localStorage["icon"] = "images/icon25_back.png";
-		    chrome.pageAction.setIcon({"tabId": tab.id, "path":localStorage["icon"]});
-		    chrome.pageAction.show(tab.id);
-		} else {
-		    localStorage["icon"] = "images/icon25.png";
-		    chrome.pageAction.setIcon({"tabId": tab.id, "path":localStorage["icon"]});
-		    chrome.pageAction.show(tab.id);
-		}
+  if (tab.url.indexOf("youtube.com") > 0) {
+    if (fullscreen) {
+      localStorage["icon"] = "images/icon25_back.png";
+      chrome.pageAction.setIcon(
+          {"tabId" : tab.id, "path" : localStorage["icon"]});
+      chrome.pageAction.show(tab.id);
+    } else {
+      localStorage["icon"] = "images/icon25.png";
+      chrome.pageAction.setIcon(
+          {"tabId" : tab.id, "path" : localStorage["icon"]});
+      chrome.pageAction.show(tab.id);
     }
+  }
 }
 
 // Called when user clicks on browser action
 chrome.pageAction.onClicked.addListener(function(tab) {
-    currentTab = tab;
-    //get the seek-time of the video if available
-    chrome.tabs.executeScript(tab.id, {file: "seek.js"});
+  currentTab = tab;
+  // get the seek-time of the video if available
+  chrome.tabs.executeScript(tab.id, {file : "seek.js"});
 });
 
 /**
  * Setup a listener for handling requests from resolution.js.
  **/
-chrome.extension.onMessage.addListener(
-    function(request, sender, sendResponse) {
-	if(request.message == "ack") {
-	    chrome.tabs.sendMessage(currentTab.id, {message: "toggleFullscreen"},
-			function(response) {
-				if (!response.success) {
-					console.log("Something wrong happens.");
-				}
-			}
-	    );
-	} else if(request.message == "stateChanged") {
-		toggleIcon(currentTab, request.action);
-	} else if(request.message == "reset") {
-	    isResUpdated = false;
-	}
-    }
-);
+chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.message == "ack") {
+    chrome.tabs.sendMessage(currentTab.id, {message : "toggleFullscreen"},
+                            function(response) {
+                              if (!response.success) {
+                                console.log("Something wrong happens.");
+                              }
+                            });
+  } else if (request.message == "stateChanged") {
+    toggleIcon(currentTab, request.action);
+  } else if (request.message == "reset") {
+    isResUpdated = false;
+  }
+});
 
 // wait for a tab to open with a youtube url.
 chrome.tabs.onUpdated.addListener(setIconForYouTube);
--- seek.js
+++ seek.js
@@ -5,98 +5,95 @@
 var fullscreen = false;
 
 // subroot parent
-function getPlayer() {
-    return document.getElementById("player-api");
-}
+function getPlayer() { return document.getElementById("player-api"); }
 
 // control many classes
 function getMoviePlayer() {
-    return document.getElementById("movie_player");
-// html5-video-player ytp-big-mode
+  return document.getElementById("movie_player");
+  // html5-video-player ytp-big-mode
 }
 
 // Video element itself
-function getVideo() {
-    return document.getElementsByTagName("video")[0];
-}
+function getVideo() { return document.getElementsByTagName("video")[0]; }
 
 // Black background
 function getPlayerHolder() {
-    return document.getElementById("player-container");
+  return document.getElementById("player-container");
 }
 
-
 function toggleFullscreen() {
-    var player = getPlayer();
-    var player_style = player.style;
-    var holder_style = getPlayerHolder().style;
-    fullscreen = !fullscreen;
-    if (fullscreen) {
-        document.body.style.overflow = "hidden"
+  var player = getPlayer();
+  var player_style = player.style;
+  var holder_style = getPlayerHolder().style;
+  fullscreen = !fullscreen;
+  if (fullscreen) {
+    document.body.style.overflow = "hidden"
 
-        player_style.position = "absolute";
-        var rect = player.getBoundingClientRect();
-        var transform_scalew = window.innerWidth / rect.width;
-        var transform_scaleh = window.innerHeight / rect.height;
-        var transform_scale = Math.min(transform_scalew, transform_scaleh);
-        player_style.transform = "scale(" + transform_scale + ", " + transform_scale +")";
-        rect = player.getBoundingClientRect();
-        var target_x = (window.innerWidth - rect.width) / 2;
-        var target_y = (window.innerHeight - rect.height) / 2;
-        //player_style.left = (target_x - rect.left) + "px";
-        player_style.transform = "translate(" + (target_x - rect.left) + "px , " +
-            (target_y - rect.top) + "px) scale(" + transform_scale + ", " + transform_scale +")";
-        player_style.zIndex = "2000000001";
+    player_style.position = "absolute";
+    var rect = player.getBoundingClientRect();
+    var transform_scalew = window.innerWidth / rect.width;
+    var transform_scaleh = window.innerHeight / rect.height;
+    var transform_scale = Math.min(transform_scalew, transform_scaleh);
+    player_style.transform =
+        "scale(" + transform_scale + ", " + transform_scale + ")";
+    rect = player.getBoundingClientRect();
+    var target_x = (window.innerWidth - rect.width) / 2;
+    var target_y = (window.innerHeight - rect.height) / 2;
+    // player_style.left = (target_x - rect.left) + "px";
+    player_style.transform = "translate(" + (target_x - rect.left) + "px , " +
+                             (target_y - rect.top) + "px) scale(" +
+                             transform_scale + ", " + transform_scale + ")";
+    player_style.zIndex = "2000000001";
 
-        holder_style.position = "absolute";
-        holder_style.transform = "scale(" + transform_scalew + ", " + transform_scaleh +")";
-        rect = getPlayerHolder().getBoundingClientRect();
-        target_x = (window.innerWidth - rect.width) / 2;
-        target_y = (window.innerHeight - rect.height) / 2;
-        holder_style.transform = "translate(" + (target_x - rect.left) + "px , " +
-            (target_y - rect.top) + "px) scale(" + transform_scalew + ", " + transform_scaleh +")";
-        holder_style.zIndex = "2000000000";
+    holder_style.position = "absolute";
+    holder_style.transform =
+        "scale(" + transform_scalew + ", " + transform_scaleh + ")";
+    rect = getPlayerHolder().getBoundingClientRect();
+    target_x = (window.innerWidth - rect.width) / 2;
+    target_y = (window.innerHeight - rect.height) / 2;
+    holder_style.transform = "translate(" + (target_x - rect.left) + "px , " +
+                             (target_y - rect.top) + "px) scale(" +
+                             transform_scalew + ", " + transform_scaleh + ")";
+    holder_style.zIndex = "2000000000";
 
-        getMoviePlayer().classList.remove("html5-video-player");
-        getMoviePlayer().classList.add("ytp-big-mode");
+    getMoviePlayer().classList.remove("html5-video-player");
+    getMoviePlayer().classList.add("ytp-big-mode");
 
-        getVideo().focus();
-        getVideo().addEventListener("ended", videoDone, true);
-    } else {
-        document.body.style.overflow = ""
+    getVideo().focus();
+    getVideo().addEventListener("ended", videoDone, true);
+  } else {
+    document.body.style.overflow = ""
 
-        player_style.transform = "";
-        player_style.zIndex = "";
-        player_style.position = "";
+    player_style.transform = "";
+    player_style.zIndex = "";
+    player_style.position = "";
 
-        holder_style.position = "";
-        holder_style.transform = "";
-        holder_style.zIndex = "";
+    holder_style.position = "";
+    holder_style.transform = "";
+    holder_style.zIndex = "";
 
-        getMoviePlayer().classList.add("html5-video-player");
-        getMoviePlayer().classList.remove("ytp-big-mode");
+    getMoviePlayer().classList.add("html5-video-player");
+    getMoviePlayer().classList.remove("ytp-big-mode");
 
-        getVideo().removeEventListener("ended", videoDone, true);
-    }
-    chrome.extension.sendMessage({message: "stateChanged", action: fullscreen});
+    getVideo().removeEventListener("ended", videoDone, true);
+  }
+  chrome.extension.sendMessage({message : "stateChanged", action : fullscreen});
 }
 
 function videoDone() {
-    console.log("videoDone.");
-    getVideo().removeEventListener("ended", videoDone, true);
-    if (fullscreen) {
-        toggleFullscreen();
-    }
+  console.log("videoDone.");
+  getVideo().removeEventListener("ended", videoDone, true);
+  if (fullscreen) {
+    toggleFullscreen();
+  }
 }
 
 // ready to process
-chrome.extension.sendMessage({message: "ack"});
+chrome.extension.sendMessage({message : "ack"});
 
-chrome.extension.onMessage.addListener(
-    function(request, sender, sendResponse) {
-	if (request.message == "toggleFullscreen") {
-        toggleFullscreen();
-	    sendResponse({success: true});
-	}
-    }
-);
+chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.message == "toggleFullscreen") {
+    toggleFullscreen();
+    sendResponse({success : true});
+  }
+});
